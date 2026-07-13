f42247270d3495ce9b48f20b089dade7ba9957fa
C Barton <cbarto8@emory.edu>
2026-07-13 13:35:02 -0400

    ci testing

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -3,7 +3,7 @@
 revisions_logfile = "/opt/blacklight-catalog/revisions.log"
 GIT_SHA =
   if Rails.env.production? && File.exist?(revisions_logfile)
-    `tail -1 #{revisions_logfile}`.chomp.split(" ")[3].gsub(/\)$/, '')
+    `tail -1 #{revisions_logfile}`.chomp.split(" ")[3].gsub(/\)$/,"")
   elsif Rails.env.development? || Rails.env.test?
     `git rev-parse HEAD`.chomp
   else
