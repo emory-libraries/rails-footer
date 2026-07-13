6e3f6a1836e87405b7d0b7588e14eadc594c6604
C Barton <cbarto8@emory.edu>
2026-07-13 14:58:37 -0400

    ci testing

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,14 +1,20 @@
 require_relative "boot"
 
+# require "rails/all"
 require "rails"
+
 # Pick the frameworks you want:
+# require "active_support/all"
 require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "action_controller/railtie"
-# require "action_mailer/railtie"
+require "action_mailer/railtie"
 require "action_view/railtie"
-require "action_cable/railtie"
+# require "action_cable/railtie"
 require "rails/test_unit/railtie"
+# require "action_text/railtie"
+# require "propshaft/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
