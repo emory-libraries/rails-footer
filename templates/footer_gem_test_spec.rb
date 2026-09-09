14c1ee4d784e3d49b481b631edb6002a97ff04ca
C Barton <cbarto8@emory.edu>
2026-09-09 12:11:41 -0400

    fixing linting

--- templates/footer_gem_test_spec.rb
+++ templates/footer_gem_test_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe "visit page, there's a footer", type: :feature do
     visit root_path
     expect(page).to have_selector(".footer-list")
   end
-
 end
 
 # test that the version line is pulling variables
